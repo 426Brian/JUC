d029a7891da9c37996f89443c238625f439fe056
426Brian <1165380405@qq.com>
2020-02-11 14:44:13 +0800

    dynamicProxyReview(important)
    Signed-off-by: 426Brian <1165380405@qq.com>

--- juc-demo01/src/test/java/com/proxy/ProxyTest.java
+++ juc-demo01/src/test/java/com/proxy/ProxyTest.java
@@ -1,55 +1,60 @@
 package com.proxy;
 
+
 import org.junit.Test;
 
-import java.io.ObjectInputStream;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 
 interface Human {
-    String getBelief();
+    String belief();
 
     void eat(String food);
 }
 
 public class ProxyTest {
     @Test
-    public void test() {
+    public void test(){
+        Human human = (Human) ProxyFactory.getProxy(new SuperMan());
 
-        Human proxy = (Human) ProxyFactory.getProxy(new SuperMan());
-
-        String belief = proxy.getBelief();
+        String belief = human.belief();
         System.out.println(belief);
     }
+
 }
 
 class SuperMan implements Human {
     @Override
-    public String getBelief() {
+    public String belief() {
         return "I believe i can fly.";
     }
 
     @Override
     public void eat(String food) {
-        System.out.println("eat + " + food);
+        System.out.println("eat food is == " + food);
     }
 }
 
 class ProxyFactory {
     public static Object getProxy(Object obj) {
-        MyHandler myHandler = new MyHandler();
-        myHandler.bind(obj);
-        return Proxy.newProxyInstance(obj.getClass().getClassLoader(), obj.getClass().getInterfaces(), myHandler);
+        MyHandler myHandler = new MyHandler(obj);
+        return Proxy.newProxyInstance(
+                obj.getClass().getClassLoader(),
+                obj.getClass().getInterfaces(),
+                myHandler
+        );
     }
 }
 
 class MyHandler implements InvocationHandler {
+    // 被代理对象
     private Object obj;
 
-    public void bind(Object obj) {
+    public MyHandler(Object obj) {
         this.obj = obj;
     }
+
 
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
